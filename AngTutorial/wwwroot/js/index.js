66fe1ac4d3748e14c20b74f59481ef90788b00d6
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-16 19:45:12 +0200

    added js and jquery

---
+++ AngTutorial/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿console.log("kappa");
+
+
+var theForm = document.getElementById("theForm");
+
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+
+var productInfo = document.getElementsByClassName("product-props");
+//productInfo.item[0].chi
