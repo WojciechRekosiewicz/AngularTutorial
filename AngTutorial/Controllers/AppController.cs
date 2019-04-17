f62ab28b6a13ad191f45abdc04a0e3a8a81a2ef3
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-17 11:34:28 +0200

    added mvc handle in startup.cs

--- AngTutorial/Controllers/AppController.cs
+++ AngTutorial/Controllers/AppController.cs
@@ -10,6 +10,8 @@ namespace AngTutorial.Controllers
     {
         public IActionResult Index()
         {
+          
+
             return View();
         }
     }
--- AngTutorial/Startup.cs
+++ AngTutorial/Startup.cs
@@ -15,6 +15,7 @@ namespace AngTutorial
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,9 +23,16 @@ namespace AngTutorial
         {
 
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
             app.UseNodeModules(env);
+            app.UseStaticFiles();
+
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
