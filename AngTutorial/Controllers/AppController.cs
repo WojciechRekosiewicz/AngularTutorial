fcd13066eef0d7abf9cdbd2af4afb5b8c53f992d
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-17 18:45:20 +0200

    modify contact.cshtml to use asp.net for and modify contactViewModel fields to required

--- AngTutorial/Controllers/AppController.cs
+++ AngTutorial/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AngTutorial.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +23,7 @@ namespace AngTutorial.Controllers
         }
 
         [HttpPost("contact")]
-        public IActionResult Contact(object model)
+        public IActionResult Contact(ContactViewModel model)
         {
             return View();
         }
---
+++ AngTutorial/ViewModels/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AngTutorial.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
