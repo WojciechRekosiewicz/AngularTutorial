afa8ead6f45158101fdbf31266eb499e145d778a
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-18 20:43:30 +0200

    created FilmContent  (DbContent)

--- AngTutorial/Data/FilmContext.cs
+++ AngTutorial/Data/FilmContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AngTutorial.Data
 {
-    public class FilmContext
+    public class FilmContext :DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
