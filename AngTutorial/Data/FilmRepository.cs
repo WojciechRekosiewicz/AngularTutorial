cede012aa0234e67467907241d60c66b27fb851a
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-19 20:44:15 +0200

    added FilmRepository

---
+++ AngTutorial/Data/FilmRepository.cs
@@ -0,0 +1,33 @@
+﻿using MovieShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.Data
+{
+    public class FilmRepository
+    {
+        private readonly FilmContext _ctx;
+
+        public FilmRepository(FilmContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _ctx.Products
+                    .OrderBy(p => p.Title)
+                    .ToList();
+        }
+
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                    .Where(p => p.Category == category)
+                    .ToList();
+        }
+    }
+}
