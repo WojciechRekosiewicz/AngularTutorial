9c4d92760f06187322248425e5dca7e8b75592e7
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-19 20:10:25 +0200

    added data to use in DB

---
+++ AngTutorial/Data/FilmSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.Data
+{
+    public class FilmSeeder
+    {
+        private readonly FilmContext _ctx;
+
+        public FilmSeeder(FilmContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        public void Seed()
+        {
+            _ctx.Database.EnsureCreated();
+
+            if (!_ctx.Products.Any())
+            {
+
+            }
+        }
+    }
+}
---
+++ AngTutorial/Data/Migrations/20190419171801_SeedData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieShop.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2019, 4, 19, 17, 18, 1, 371, DateTimeKind.Utc));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2019, 4, 19, 17, 16, 37, 270, DateTimeKind.Utc));
+        }
+    }
+}
