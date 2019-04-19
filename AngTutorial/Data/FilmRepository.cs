9137b91ef53ee8622532b43baf39b68d6ab6bae5
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-19 21:37:33 +0200

    add error handling to GetAllProducts in FilmRepository

--- AngTutorial/Data/FilmRepository.cs
+++ AngTutorial/Data/FilmRepository.cs
@@ -1,4 +1,5 @@
-﻿using MovieShop.Data.Entities;
+﻿using Microsoft.Extensions.Logging;
+using MovieShop.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,29 @@ namespace MovieShop.Data
     public class FilmRepository : IFilmRepository
     {
         private readonly FilmContext _ctx;
+        private readonly ILogger<FilmRepository> _logger;
 
-        public FilmRepository(FilmContext ctx)
+        public FilmRepository(FilmContext ctx, ILogger<FilmRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products
-                    .OrderBy(p => p.Title)
-                    .ToList();
+            try
+            {
+                _logger.LogInformation("GetAllProducts was called");
+                return _ctx.Products
+                   .OrderBy(p => p.Title)
+                   .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products! {ex}");
+                return null;
+            }
+           
         }
 
 
