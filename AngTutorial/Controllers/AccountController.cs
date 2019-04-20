bf7310e38118684151ffcb25b8542db9549caad8
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-20 22:34:53 +0200

    login works

--- AngTutorial/Controllers/AccountController.cs
+++ AngTutorial/Controllers/AccountController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MovieShop.Data.Entities;
+using MovieShop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,10 +15,13 @@ namespace MovieShop.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
 
@@ -28,5 +34,34 @@ namespace MovieShop.Controllers
 
             return View();
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+
+                if(result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                       return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+
+            ModelState.TryAddModelError("", "Failed to login");
+
+            return View();
+        }
+
     }
 }
