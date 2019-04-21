cae545ad9d8cb4133765ed59e6ce40b29ac3388d
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-21 17:38:18 +0200

    start adding angular to project

---
+++ AngTutorial/film-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center">
+      <h1>
+        Welcome to {{title}}!
+      </h1>  
+    </div>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Movie Shop';
+}
