a9d5534cc8517dadafd17e1888eb8db3c22d333d
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-04-21 22:16:15 +0200

    start integrating angular to the project and start replacing razor pages with angular

--- AngTutorial/ClientApp/app/app.component.ts
+++ AngTutorial/ClientApp/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-root',
+  selector: 'the-shop',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
       <h1>
         Welcome to {{title}}!
       </h1>  
-    </div>
   `,
   styles: []
 })
---
+++ AngTutorial/wwwroot/clientapp/out-tsc/ClientApp/app/app.component.js
@@ -0,0 +1,17 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'Movie Shop';
+    }
+    AppComponent = tslib_1.__decorate([
+        Component({
+            selector: 'the-shop',
+            template: "\n      <h1>\n        Welcome to {{title}}!\n      </h1>  \n  ",
+            styles: []
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
